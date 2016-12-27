a06a991b37a32473f7ffa601e89c180996921555
MarcinDuraj <DurajMarcin42@gmail.com>
2016-12-27 15:30:54 +0100

    Add class FlyNoWay, implementation of FlyBehavior

---
+++ StrategyPattern/StrategyPattern/FlyNoWay.cpp
@@ -0,0 +1,17 @@
+#include "FlyNoWay.h"
+#include<iostream>
+
+
+FlyNoWay::FlyNoWay()
+{
+}
+
+
+FlyNoWay::~FlyNoWay()
+{
+}
+
+void FlyNoWay::fly()
+{
+    std::cout << "I'm not flying" << std::endl;
+}
---
+++ StrategyPattern/StrategyPattern/FlyNoWay.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "FlyBehavior.h"
+class FlyNoWay :
+    public FlyBehavior
+{
+public:
+    FlyNoWay();
+    ~FlyNoWay();
+
+    virtual void fly();
+};
+
